2cf8eb7913e554e90566f4c85b6b8fcdef2fb4f7
ericson.veliz.contractor <ericson.veliz.contractor@bbva.com>
2024-03-01 19:18:05 -0500

    ÂAgregando interface ProductRepository y Configuraciones

---
+++ src/main/java/com/evelizp/repository/ProductRepository.java
@@ -0,0 +1,19 @@
+package com.evelizp.repository;
+
+import com.evelizp.model.entity.CategoryEntity;
+import com.evelizp.model.entity.DeletedProduct;
+import com.evelizp.model.entity.ProductEntity;
+import com.evelizp.model.entity.ProductStatus;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ProductRepository extends CrudRepository<ProductEntity, Long> {
+
+    @Query("from ProductEntity where deleted = com.codearti.model.entity.DeletedProduct.CREATED and ((:status is null) or (status = :status))")
+    List<ProductEntity> findAll(@Param("status") ProductStatus status);
+
+    List<ProductEntity> findByCategoryAndDeleted(CategoryEntity category, DeletedProduct deleted);
+}
